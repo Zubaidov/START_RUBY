efaacba685136c9890be29712c39a3aff3802919
Zubaidov <shohruz.zubaidov@gmail.com>
2023-08-24 22:57:25 +0400

    if condition

--- condition.rb
+++ condition.rb
@@ -1,3 +1,15 @@
 and_check = true && false
 or_check = true || false
 p and_check, or_check
+
+grade = gets.chomp.to_i # Gets the input and converts to int
+
+if grade>=90
+    puts "A+"
+    
+elsif grade >= 80 && grade <= 89
+    puts "A"
+    
+else
+    puts "B"
+end
--- operators.rb
+++ operators.rb
@@ -6,7 +6,3 @@ p a-b
 p a/b
 p a*b
 p a%b
-
-and_check = true && false
-or_check = true || false
-p and_check, or_check
