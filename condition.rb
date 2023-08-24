a0ab9a017666eefd0e0e8ad9210c542d201690af
Zubaidov <shohruz.zubaidov@gmail.com>
2023-08-24 22:52:22 +0400

    operators and conditions

---
+++ condition.rb
@@ -0,0 +1,3 @@
+and_check = true && false
+or_check = true || false
+p and_check, or_check
---
+++ operators.rb
@@ -0,0 +1,12 @@
+a = 100
+b = 50
+
+p a+b
+p a-b
+p a/b
+p a*b
+p a%b
+
+and_check = true && false
+or_check = true || false
+p and_check, or_check
